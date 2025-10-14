835affe6790e949ed11212bf22c146a25045a4fb
aditya singh rathore <rathoreadityasingh40@gmail.com>
2025-10-14 10:36:31 +0530

    Add linke refernce in footer Documentations

--- src/theme/Footer/Layout/index.tsx
+++ src/theme/Footer/Layout/index.tsx
@@ -242,7 +242,7 @@ export default function FooterLayout({
               </h3>
               <ul className="footer-links">
                 <li>
-                  <Link to="#" className="footer-link">
+                  <Link to="/docs" className="footer-link">
                     <span className="link-icon">📖</span>
                     Documentation
                     <span className="link-badge popular">Popular</span>
