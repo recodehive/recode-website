230549d19ac9438f6d101494c922dccffaf59c20
Srushti-Kamble <kamblesrushti127@gmail.com>
2026-05-28 23:02:26 +0530

    Fixed event listener for continous autoplay

--- src/components/blogCarousel/blogCarousel.tsx
+++ src/components/blogCarousel/blogCarousel.tsx
@@ -27,9 +27,15 @@ export function BlogCarousel() {
     setCount(api.scrollSnapList().length);
     setCurrent(api.selectedScrollSnap() + 1);
 
-    api.on("select", () => {
+    const onSelect = () => {
       setCurrent(api.selectedScrollSnap() + 1);
-    });
+    };
+
+    api.on("select", onSelect);
+
+    return () => {
+      api.off("select", onSelect);
+    };
   }, [api]);
 
   return (
@@ -62,8 +68,9 @@ export function BlogCarousel() {
         }}
         plugins={[
           Autoplay({
-            delay: 4000,
-            stopOnInteraction: true,
+            delay: 5000,
+            stopOnInteraction: false,
+            stopOnMouseEnter: true,
           }),
         ]}
       >
