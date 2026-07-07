8a127cbdba64fbd8701f0a920a14b588cd058993
aaniya22 <aaniyaatomar@gmail.com>
2026-07-07 12:28:55 +0530

    fix: mount DailyCodingTip widget on the homepage
    The widget was fully built (component, styles, localStorage persistence,
    refresh button) but was only wired into the Leaderboard page, not the
    homepage as the linked issue requested. Import and render
    DailyCodingTip on src/pages/index.tsx, directly below the header.
    
    Signed-off-by: aaniya22 <aaniyaatomar@gmail.com>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -14,6 +14,7 @@ import { TestimonialCarousel } from "../components/testimonials/TestimonialCarou
 import { CommunityStatsProvider } from "../lib/statsProvider";
 import { LandingCommunity } from "../components/Community";
 import FAQs from "../components/faqs/faqs";
+import DailyCodingTip from "../components/DailyCodingTip";
 
 export default function Home(): ReactNode {
   const { siteConfig } = useDocusaurusContext();
@@ -66,6 +67,10 @@ export default function Home(): ReactNode {
         >
           <div className="m-4">
             <Header />
+          </div>
+
+          <div className="m-4">
+            <DailyCodingTip />
           </div>
 
           <div className="blog-carousel-section">
